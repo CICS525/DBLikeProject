d5bbb0b365794d90d08ac7c52ef8df3d474b7ea3
Eli Dunn <elitward@gmail.com>
2014-07-07 01:52:19 -0700

    Updating skeleton.

--- FolderSync/Client/MetadataManager.cs
+++ FolderSync/Client/MetadataManager.cs
@@ -10,7 +10,7 @@ namespace Client
 {
     class MetadataManager
     {
-        private static ulong sGlobalWriteCounter = 0;    //this counter recorders how many write recording has been added on master server. The value is up to master server. Client only save the last value to know how big is the gap since last sync.
+        private static ulong sGlobalWriteCounter = 0;               //this counter recorders how many write recording has been added on master server. The value is up to master server. Client only save the last value to know how big is the gap since last sync.
         private static List<FileMetadata> sLocalMetadata = null;    //this list cache all metadata for every file under root dir
         private static MetadataManager that = null;
 
@@ -19,13 +19,14 @@ namespace Client
             //singleton design pattern
             if(sLocalMetadata==null)
             {
-                sLocalMetadata = ReadLocalMetadata();
+                ReadLocalMetadata();
             }
         }
 
-        public List<FileMetadata> ReadLocalMetadata()
+        public bool ReadLocalMetadata()
         {
-            return null;
+            sLocalMetadata = new List<FileMetadata>();  //update it
+            return false;
         }
 
         public void SaveLocalMetadate()
@@ -35,6 +36,29 @@ namespace Client
         public MetadataManager GetInstance()
         {
             if(that==null)
+            {
+                that = new MetadataManager();
+            }
+            return that;
+        }
+
+        public List<FileMetadata> GetLocalMetadata()
+        {
+            return sLocalMetadata;
+        }
+
+        public bool UpdateMetadata(FileMetadata aFileMetadata)  // local metadata should be udate on create / update a file. There metadata is from master server, for the SHA is calculated on master server.
+        {
+            //overwrite into sLocalMetadata
+            SaveLocalMetadate();
+            return false;
+        }
+
+        public bool DeleteMetadate(FileMetadata aFileMetadata)  // local metadata should be udate on delete a file
+        {
+            //delete from sLocalMetadata
+            SaveLocalMetadate();
+            return false;
         }
     }
 }
