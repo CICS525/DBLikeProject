f5b24f60188aae7930986a922e6bff60684a0f2c
Sky <dtlsky@gmail.com>
2014-07-16 09:37:20 -0700

    Edit deleteFile and prepareFolder

--- CloudSync/Client/src/cloudsync/client/FileSysPerformer.java
+++ CloudSync/Client/src/cloudsync/client/FileSysPerformer.java
@@ -26,18 +26,48 @@ public class FileSysPerformer {
 	
 	private boolean prepareFolder(String filename){
 		//maybe need to create directory before writing
+		String folder = filename.substring(0, filename.lastIndexOf(File.separator));
+		File directory = new File(folder);
+		if(!directory.exists()){
+			System.out.println("Creating a directory " + folder);
+			boolean isCreated = false;
+			try {
+				directory.mkdir();
+				isCreated = true;
+			} catch (SecurityException se) {
+				System.out.println(se.getMessage());
+			}
+			if (isCreated) {
+				System.out.println("Directory " + folder + " is created");
+			}
+		}
 		return false;
 	}
 	
 	private boolean deleteFile(String filename){
+		File file = new File(filename);
+		if (file.delete()){
+			System.out.println(file.getName() + " is deleted.");
+		} else {
+			System.out.println("Delete operation fails");
+		}
 		// delete empty folder when it is empty
-		// Get the folder from filename
-		String director = filename.substring(0,filename.lastIndexOf(File.separator));
+		String folder = filename.substring(0,filename.lastIndexOf(File.separator));
+		File directory = new File(folder);
+		if (directory.isDirectory()){
+			if(directory.list().length>0){
+				System.out.println("Directory is not empty");
+			} else {
+				directory.delete();
+			}	
+		} else {
+			System.out.println("This is not a folder");
+		}
 		return false;
 	}
 
 	private boolean FilePerform(Metadata metadata){
-		// Add this file to the ignore list of its FileSysMonitor
+		// Add this file to the ignore list of all FileSysMonitors
 		for(FileSysMonitor aMonitor : ClientMain.getAllFileMonitors()){
 			aMonitor.startIgnoreFile(metadata.filename);
 		}
@@ -50,7 +80,7 @@ public class FileSysPerformer {
 			blobSession.downloadFile(metadata);
 		}
 		
-		// Remove this file from the ignore list of its FileSysMonitor 
+		// Remove this file from the ignore list all FileSysMonitors 
 		for(FileSysMonitor aMonitor : ClientMain.getAllFileMonitors()){
 			aMonitor.stopIgnoreFile(metadata.filename);
 		}
