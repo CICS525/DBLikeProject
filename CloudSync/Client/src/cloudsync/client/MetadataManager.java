bd9f74036cfc0c213c806ebb120e41846acded5b
achengscode <docaholic@gmail.com>
2014-07-17 09:36:58 -0700

    filling in MetadataManager

--- CloudSync/Client/src/cloudsync/client/MetadataManager.java
+++ CloudSync/Client/src/cloudsync/client/MetadataManager.java
@@ -1,14 +1,27 @@
 package cloudsync.client;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import cloudsync.sharedInterface.Metadata;
 
+/**
+ * Class that manages the all the metadata for
+ * a particular client.
+ * @author Aaron Cheng
+ *
+ */
 public class MetadataManager {
 
 	private long GlobalWriteCounter;
 	private ArrayList<Metadata> LocalMetadata;
 	private static MetadataManager that = null;
+	private static String META_FILENAME = "metadata.dat";
 	
 	private MetadataManager(){
 		//private constructor to secure singleton
@@ -29,13 +42,31 @@ public class MetadataManager {
 		return LocalMetadata;
 	}
 
-	public boolean readLocalMetadata(){
+	/**
+	 * Reads the local metadata information from the file
+	 * @return
+	 */
+	public boolean readLocalMetadata() throws FileNotFoundException, IOException {
 		GlobalWriteCounter = 1; //should be read from local file
+		Scanner scan = new Scanner(META_FILENAME);
+		
 		LocalMetadata = new ArrayList<Metadata>();
 		return false;
 	}
 	
-	public boolean saveLocalMetadate(){
+	/**
+	 * Saves the list of metadata to the file.
+	 * @return
+	 */
+	public boolean saveLocalMetadate() throws FileNotFoundException, IOException {
+		File file = new File(META_FILENAME);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		writer.write(String.valueOf(GlobalWriteCounter) + "\n");
+		for (Metadata m: LocalMetadata) {
+			writer.write(String.valueOf(m.status));
+			writer.write(String.valueOf(m.parent));
+			writer.write(m.blobKey);
+		}
 		return false;
 	}
 
