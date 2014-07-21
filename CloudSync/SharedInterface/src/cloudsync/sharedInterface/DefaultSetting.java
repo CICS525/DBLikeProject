15a2d0ca44d8ea4a0088927cd7164defb292f3c9
Eli Dunn <elitward@gmail.com>
2014-07-20 21:47:03 -0700

    Add TCP pop info in comment

--- CloudSync/SharedInterface/src/cloudsync/sharedInterface/DefaultSetting.java
+++ CloudSync/SharedInterface/src/cloudsync/sharedInterface/DefaultSetting.java
@@ -1,6 +1,8 @@
 package cloudsync.sharedInterface;
 
 public class DefaultSetting {
+	//TCP: IMAP:143	/ LDAP:389 / POP3:110
+	
 	public final static String	DEFAULT_MASTER_SERVER_URL = "127.0.0.1";
 	public final static int		DEFAULT_MASTER_SERVEL_PORT = 389;	//use LDAP default port number to facilitate Azure Virtual Machine configure
 	
