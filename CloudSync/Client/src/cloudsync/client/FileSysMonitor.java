f3deebad99cb0c2e8f5fd4f56fb635318e7b2464
Eli Dunn <elitward@gmail.com>
2014-07-22 21:49:30 -0700

    add debug message in FileSysMonitor

--- CloudSync/Client/src/cloudsync/client/FileSysMonitor.java
+++ CloudSync/Client/src/cloudsync/client/FileSysMonitor.java
@@ -75,7 +75,6 @@ public class FileSysMonitor {
 								Path child = (Path) event.context();
 								File currFile = child.toFile();
 								newTimeStamp = currFile.lastModified();
-								System.out.print("watcher: newTimeStamp = " + newTimeStamp + " ~ ");
 								String filename = child.toAbsolutePath().toString();
 								if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
 									registerSubfolders(child);
@@ -86,22 +85,21 @@ public class FileSysMonitor {
 									Action action = FileSysMonitorCallback.Action.ERROR;
 									Kind<?> type = event.kind();
 									if (type == StandardWatchEventKinds.ENTRY_CREATE) {
-										System.out.print("ENTRY_CREATE -> MODIFY ");
+										System.out.println("FileSysMonitor: newTimeStamp = " + newTimeStamp + " ~ ENTRY_CREATE -> MODIFY ");
 										action = FileSysMonitorCallback.Action.MODIFY;
 									} else if ((type == StandardWatchEventKinds.ENTRY_MODIFY) && (newTimeStamp > oldTimeStamp)) {
-										System.out.print("ENTRY_MODIFY -> MODIFY ");
+										System.out.println("FileSysMonitor: newTimeStamp = " + newTimeStamp + " ~ ENTRY_MODIFY -> MODIFY ");
 										action = FileSysMonitorCallback.Action.MODIFY;
 									} else if (type == StandardWatchEventKinds.ENTRY_MODIFY && (newTimeStamp <= oldTimeStamp)) {
-										System.out.println("ENTRY_MODIFY -> continue ");
+										System.out.println("FileSysMonitor: newTimeStamp = " + newTimeStamp + " ~ ENTRY_MODIFY -> continue ");
 										continue;
 									} else if (type == StandardWatchEventKinds.ENTRY_DELETE) {
-										System.out.print("ENTRY_DELETE -> DELETE ");
+										System.out.println("FileSysMonitor: newTimeStamp = " + newTimeStamp + " ~ ENTRY_DELETE -> DELETE ");
 										action = FileSysMonitorCallback.Action.DELETE;
 									}
 									callback.Callback(child.toAbsolutePath().toString(), action);
 								}
 								oldTimeStamp = newTimeStamp;
-								System.out.println("...");
 							}
 							boolean valid = key.reset();
 							if (!valid) {
