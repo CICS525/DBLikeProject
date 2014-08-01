70f152c8975b182657d737a8b593c41177f95541
Sky <dtlsky@gmail.com>
2014-08-01 15:50:02 -0700

    Modify the test for metalist

--- CloudSync/Client/src/test/FileSysPerformerTest.java
+++ CloudSync/Client/src/test/FileSysPerformerTest.java
@@ -4,11 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cloudsync.client.ClientSettings;
 import cloudsync.client.FileSysPerformer;
 import cloudsync.sharedInterface.Metadata;
 import cloudsync.sharedInterface.Metadata.STATUS;
 
 public class FileSysPerformerTest {
+	/*
 	@Test
 	public void testGetAbsoluteFilename() {
 		FileSysPerformer fileSysPerformer = null;
@@ -23,15 +25,27 @@ public class FileSysPerformerTest {
 		String bsName = fileSysPerformer.getBaseFilename("C:\\Users\\Tianlai Dong\\Desktop\\test.docx");
 		assertEquals("Desktop\\test.docx",bsName);
 	}
+	*/
+	
 	@Test 
 	public void testAddUpdateLocalTask(){
-		FileSysPerformer fileSysPerformer = null;
-		fileSysPerformer = FileSysPerformer.getInstance();
-		Metadata metadata = new Metadata("Desktop\\test.docx");
+		ClientSettings cSettings = ClientSettings.getInstance();
+		cSettings.setRootDir("C:\\Users\\Tianlai Dong");
+		FileSysPerformer fileSysPerformer = FileSysPerformer.getInstance();
+		Metadata metadata = new Metadata("Desktop\\1234.txt");
+		Metadata metadata2 = new Metadata("Desktop\\123.txt");
+		System.out.println(metadata.basename);
 		fileSysPerformer.addUpdateLocalTask(metadata);
+		fileSysPerformer.addUpdateLocalTask(metadata2);
 		System.out.println(metadata.status);
 		metadata.status = STATUS.DELETE;
 		fileSysPerformer.addUpdateLocalTask(metadata);
 		System.out.println(metadata.status);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
