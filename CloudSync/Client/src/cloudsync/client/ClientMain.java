44215f114be8fbf9f2087e9d594c0a474596829c
Eli Dunn <elitward@gmail.com>
2014-08-03 16:46:51 -0700

    auto format ClientMain.java

--- CloudSync/Client/src/cloudsync/client/ClientMain.java
+++ CloudSync/Client/src/cloudsync/client/ClientMain.java
@@ -20,85 +20,89 @@ import cloudsync.sharedInterface.SocketMessage.COMMAND;
 
 public class ClientMain {
 
-	private static final int 					RETRY_INTERVAL	= (5*1000);			
+	private static final int					RETRY_INTERVAL	= (5 * 1000);
 	private static ClientSettings				settings		= null;
 	private static ServerLocation				masterLocation	= null;
 	private static SessionMaster				masterSession	= null;
 	private static ArrayList<FileSysMonitor>	allFileMonitors	= new ArrayList<FileSysMonitor>();
 	private static MetadataManager				metadataManager	= null;
-	private static ArrayList<Operation>			delayOperations = new ArrayList<Operation>();
+	private static ArrayList<Operation>			delayOperations	= new ArrayList<Operation>();
 	private static RetryThread					commitRetry		= null;
-	
-	private static FileSysMonitorCallback		fileSysAnswer = new FileSysMonitorCallback() {
-		@Override
-		public void Callback(final Operation operation) {
-			
-			final FileSysPerformer fp = FileSysPerformer.getInstance();
-			final String absoluteFilename = fp.getAbsoluteFilename(operation.filename);
-			final String basename = fp.getBaseFilename(operation.filename);
-			if (Action.MODIFY == operation.action) {
-				System.out.println("ClientMain: FileSysMonitor~Callback: Upload File:" + absoluteFilename);
-				//LoggerClass.writeLog("ClientMain: FileSysMonitor~Callback: Upload File:" + absoluteFilename);
 
+	private static FileSysMonitorCallback		fileSysAnswer	= new FileSysMonitorCallback() {
+		
+			@Override
+			public void Callback(final Operation operation) {
 
-				if( masterLocation==null ){
-					synchronized (delayOperations){
-						delayOperations.add(operation);	//save for next try
-					}
-					return;
-				}
-				
-				fp.addDelayPerformFile(basename);
-				
-				FileSender sender = new FileSender(masterLocation.url, absoluteFilename, new FileSysCallback() {
+				final FileSysPerformer fp = FileSysPerformer.getInstance();
+				final String absoluteFilename = fp.getAbsoluteFilename(operation.filename);
+				final String basename = fp.getBaseFilename(operation.filename);
+				if (Action.MODIFY == operation.action) {
+					System.out.println("ClientMain: FileSysMonitor~Callback: Upload File:"
+							+ absoluteFilename);
+					// LoggerClass.writeLog("ClientMain: FileSysMonitor~Callback: Upload File:" + absoluteFilename);
 
-					@Override
-					public void onFinish(boolean success, String tempFileOnServer) {
-						boolean done = false;
-						if (success) {
-							done = commitFileUpdate(Action.MODIFY, absoluteFilename, tempFileOnServer);
+					if (masterLocation == null) {
+						synchronized (delayOperations) {
+							delayOperations.add(operation); // save for next try
 						}
-						if(!done){
-							synchronized (delayOperations){
-								delayOperations.add(operation);	//save for next try
-							}
-						}
-						fp.removeDelayPerformFile(basename);
+						return;
 					}
 
-				});
-				sender.startFileTransfer();
+					fp.addDelayPerformFile(basename);
 
-			} else if (Action.DELETE == operation.action) {
-				System.out.println("ClientMain: FileSysMonitor~Callback: Delete File:" + absoluteFilename);
-				boolean done = false;
-				done = commitFileUpdate(Action.DELETE, absoluteFilename, null);
-				if(!done){
-					synchronized (delayOperations){
-						delayOperations.add(operation);	//save for next try
+					FileSender sender = new FileSender(masterLocation.url, absoluteFilename,
+							new FileSysCallback() {
+
+								@Override
+								public void onFinish(boolean success, String tempFileOnServer) {
+									boolean done = false;
+									if (success) {
+										done = commitFileUpdate(Action.MODIFY, absoluteFilename,
+												tempFileOnServer);
+									}
+									if (!done) {
+										synchronized (delayOperations) {
+											delayOperations.add(operation); // save for next try
+										}
+									}
+									fp.removeDelayPerformFile(basename);
+								}
+
+							});
+					sender.startFileTransfer();
+
+				} else if (Action.DELETE == operation.action) {
+					System.out.println("ClientMain: FileSysMonitor~Callback: Delete File:"
+							+ absoluteFilename);
+					boolean done = false;
+					done = commitFileUpdate(Action.DELETE, absoluteFilename, null);
+					if (!done) {
+						synchronized (delayOperations) {
+							delayOperations.add(operation); // save for next try
+						}
 					}
 				}
 			}
-		}
-	};
-	
+		};
+
 	private static class RetryThread extends Thread {
 
 		@Override
 		public void run() {
-			while(delayOperations!=null){
+			while (delayOperations != null) {
 				try {
 					Thread.sleep(RETRY_INTERVAL);
 				} catch (InterruptedException e) {
-					//e.printStackTrace();
+					// e.printStackTrace();
 				}
-				
-				if( !SessionMaster.getInstance().checkSocketStreamMessageActive() )
+
+				if (!SessionMaster.getInstance().checkSocketStreamMessageActive())
 					initClientMain();
-				
-				synchronized(delayOperations){
+
+				synchronized (delayOperations) {
 					int len = delayOperations.size();
-					for(int i=0; i<len; i++){
+					for (int i = 0; i < len; i++) {
 						Operation op = delayOperations.remove(0);
 						System.out.println("RetryThread:" + op.filename + "#" + op.action);
 						fileSysAnswer.Callback(op);
@@ -107,7 +111,7 @@ public class ClientMain {
 			}
 			super.run();
 		}
-		
+
 	}
 
 	public static ArrayList<FileSysMonitor> getAllFileMonitors() {
@@ -130,8 +134,8 @@ public class ClientMain {
 		ClientMain.masterSession = masterSession;
 	}
 
-	private static String getHostname(){
-		
+	private static String getHostname() {
+
 		InetAddress inetAddr = null;
 		try {
 			inetAddr = InetAddress.getLocalHost();
@@ -141,14 +145,12 @@ public class ClientMain {
 		String hostname = inetAddr.getHostName();
 		return hostname;
 	}
-	
+
 	public static synchronized boolean initClientMain() {
 		System.out.println("ClientMain starts ...");
-		//LoggerClass.writeLog("ClientMain starts ...");
-
+		// LoggerClass.writeLog("ClientMain starts ...");
 
 		settings = ClientSettings.getInstance();
-		
 
 		// Client should do upload first & do download.
 		// This is in order to handle the file could be modified when the client
@@ -157,74 +159,74 @@ public class ClientMain {
 		// in local metadata.
 
 		// masterLocation = settings.getRecentMaster();
-		if(settings.loadSettings())
-		{
-		SessionEntry entry = SessionEntry.getInstance();
-		masterLocation = entry.getMasterServerLocation(settings.getUsername(), settings.getPassword());
-		if (masterLocation == null) {
-			System.out.println("Can not locate Master Server");
-			//LoggerClass.writeLog("Can not locate Master Server");
+		if (settings.loadSettings()) {
+			SessionEntry entry = SessionEntry.getInstance();
+			masterLocation = entry.getMasterServerLocation(settings.getUsername(), settings.getPassword());
+			if (masterLocation == null) {
+				System.out.println("Can not locate Master Server");
+				// LoggerClass.writeLog("Can not locate Master Server");
 
-			return false;
-		}
+				return false;
+			}
 
-		masterSession = SessionMaster.getInstance();
-		masterSession.setMasterServerLocation(masterLocation);
+			masterSession = SessionMaster.getInstance();
+			masterSession.setMasterServerLocation(masterLocation);
 
-		metadataManager = MetadataManager.getInstance();
-		metadataManager.readLocalMetadata();
+			metadataManager = MetadataManager.getInstance();
+			metadataManager.readLocalMetadata();
 
-		if( allFileMonitors.size()==0 ){	//no monitor set yet
-			FileSysMonitor fileMonitor = new FileSysMonitor(settings.getRootDir());
-			if( fileMonitor!=null ){
-				boolean bMnt = fileMonitor.startListen(fileSysAnswer);
-				if (bMnt) {
-					System.out.println("initClientMain@ClientMain: fileMonitor.StartListen#" + settings.getRootDir() + "->" + bMnt);
-					//LoggerClass.writeLog("initClientMain@ClientMain: fileMonitor.StartListen#" + settings.getRootDir() + "->" + bMnt);
+			if (allFileMonitors.size() == 0) { // no monitor set yet
+				FileSysMonitor fileMonitor = new FileSysMonitor(settings.getRootDir());
+				if (fileMonitor != null) {
+					boolean bMnt = fileMonitor.startListen(fileSysAnswer);
+					if (bMnt) {
+						System.out.println("initClientMain@ClientMain: fileMonitor.StartListen#" + settings.getRootDir() + "->" + bMnt);
+						// LoggerClass.writeLog("initClientMain@ClientMain: fileMonitor.StartListen#"
+						// + settings.getRootDir() + "->" + bMnt);
 
-					allFileMonitors.add(fileMonitor);
+						allFileMonitors.add(fileMonitor);
+					}
 				}
 			}
-		}
 
-		System.out.println("initClientMain@ClientMain: Connecint to Master Server: " + settings.getUsername() + "#" + settings.getPassword());
-		//LoggerClass.writeLog("initClientMain@ClientMain: Connecint to Master Server: " + settings.getUsername() + "#" + settings.getPassword());
+			System.out.println("initClientMain@ClientMain: Connecint to Master Server: " + settings.getUsername() + "#" + settings.getPassword());
+			// LoggerClass.writeLog("initClientMain@ClientMain: Connecint to Master Server: "
+			// + settings.getUsername() + "#" + settings.getPassword());
 
-		boolean bCnt = masterSession.connect(settings.getUsername(), settings.getPassword());
-		
-		if(bCnt && commitRetry==null){	//start retry thread
-			commitRetry = new RetryThread();
-			commitRetry.start();
-		}
-		
-		boolean bLan = FileSenderClient.initialize(DefaultSetting.DEFAULT_CLIENT_DOWNLOAD_PORT, settings.getRootDir());
-		if(bLan)
-			System.out.println("initClientMain@ClientMain: FileSenderClient Success.");
-		else
-			System.out.println("initClientMain@ClientMain: FileSenderClient Failure.");
+			boolean bCnt = masterSession.connect(settings.getUsername(), settings.getPassword());
 
-		return bCnt;
-		}else
-		{
+			if (bCnt && commitRetry == null) { // start retry thread
+				commitRetry = new RetryThread();
+				commitRetry.start();
+			}
+
+			boolean bLan = FileSenderClient.initialize(DefaultSetting.DEFAULT_CLIENT_DOWNLOAD_PORT, settings.getRootDir());
+			if (bLan)
+				System.out.println("initClientMain@ClientMain: FileSenderClient Success.");
+			else
+				System.out.println("initClientMain@ClientMain: FileSenderClient Failure.");
+
+			return bCnt;
+		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	public static synchronized boolean deinitClientMain() {
-		
-		//disable file system monitor
-		for(FileSysMonitor monitor : allFileMonitors){
+
+		// disable file system monitor
+		for (FileSysMonitor monitor : allFileMonitors) {
 			monitor.stopListen();
 		}
 		allFileMonitors.clear();
-		
-		//disable master session
+
+		// disable master session
 		masterSession.disconnect();
-		
-		//disable LAN sync listener
+
+		// disable LAN sync listener
 		FileSenderClient.deinitialize();
-		
+
 		return true;
 	}
 
@@ -236,12 +238,12 @@ public class ClientMain {
 		// --- [should not change, unless conflict] ---
 		incomplete.basename = FileSysPerformer.getInstance().getBaseFilename(absoluteFilename);
 		Metadata parentMeta = metadataManager.findByBasename(incomplete.basename);
-		if (parentMeta == null || parentMeta.status!=STATUS.LAST)
+		if (parentMeta == null || parentMeta.status != STATUS.LAST)
 			incomplete.parent = 0;
 		else
 			incomplete.parent = parentMeta.globalCounter;
 		// --- [to be over written by Master Server] ---
-		incomplete.globalCounter = 0;	//metadataManager.getSyncedGlobalWriteCounter();
+		incomplete.globalCounter = 0; // metadataManager.getSyncedGlobalWriteCounter();
 		incomplete.status = Action.MODIFY == action ? STATUS.LAST : STATUS.DELETE;
 		// incomplete.timestamp = new Date();
 		// --- [to be set by Master Server] ---
@@ -251,11 +253,16 @@ public class ClientMain {
 
 		while (true) {
 			Metadata complete = masterSession.rmiCommitFileUpdate(incomplete, tempFileOnServer);
-			//SystemTrayImplementor.displayMessage("Syncing with Cloud Folder", complete.basename, MessageType.INFO);
+			// SystemTrayImplementor.displayMessage("Syncing with Cloud Folder",
+			// complete.basename, MessageType.INFO);
 
 			if (complete != null) {
-				System.out.println("commitFileUpdate@ClientMain:" + "basename=" + complete.basename + " parent=" + complete.parent + " globalCounter=" + complete.globalCounter + " status=" + complete.status);
-				//LoggerClass.writeLog("commitFileUpdate@ClientMain:" + "basename=" + complete.basename + " parent=" + complete.parent + " globalCounter=" + complete.globalCounter + " status=" + complete.status);
+				System.out.println("commitFileUpdate@ClientMain:" + "basename=" + complete.basename + " parent=" + complete.parent + " globalCounter="
+						+ complete.globalCounter + " status=" + complete.status);
+				// LoggerClass.writeLog("commitFileUpdate@ClientMain:" +
+				// "basename=" + complete.basename + " parent=" +
+				// complete.parent + " globalCounter=" + complete.globalCounter
+				// + " status=" + complete.status);
 
 				if (complete.status == STATUS.CONFLICT) {
 					// should rename & try again in next FileSysMonitor callback
@@ -284,22 +291,24 @@ public class ClientMain {
 					}
 
 					boolean get = fOld.renameTo(fNew);
-					if(get)
+					if (get)
 						System.out.println("commitFileUpdate@ClientMain: CONFLICT~RENAME:" + absoluteFilename + " -> " + temp);
 
-					//for (FileSysMonitor aMonitor : ClientMain.getAllFileMonitors()) {
-					//	aMonitor.stopIgnoreFile(absoluteFilename);
-					//}
+					// for (FileSysMonitor aMonitor :
+					// ClientMain.getAllFileMonitors()) {
+					// aMonitor.stopIgnoreFile(absoluteFilename);
+					// }
 					return true;
 				} else if (complete.status == STATUS.ERROR) {
 					return false;
 				} else {
 					boolean suc = metadataManager.updateLocalMetadata(complete);
-					System.out.println("commitFileUpdate@ClientMain: updateLocalMetadata(" + "basename=" + complete.basename + " parent=" + complete.parent + " globalCounter=" + complete.globalCounter + " status=" + complete.status + ") => " + suc);
-					if(suc){
-						if(complete.globalCounter == 1+metadataManager.getSyncedGlobalWriteCounter())
+					System.out.println("commitFileUpdate@ClientMain: updateLocalMetadata(" + "basename=" + complete.basename + " parent=" + complete.parent
+							+ " globalCounter=" + complete.globalCounter + " status=" + complete.status + ") => " + suc);
+					if (suc) {
+						if (complete.globalCounter == 1 + metadataManager.getSyncedGlobalWriteCounter())
 							metadataManager.setSyncedGlobalWriteCounter(complete.globalCounter);
-						
+
 						SocketMessage message = new SocketMessage(COMMAND.UPDATE);
 						message.infoLong = complete.globalCounter;
 						// Get the local IP address from this machine;
@@ -309,19 +318,19 @@ public class ClientMain {
 					}
 					return suc;
 				}
-			}else{
-				System.out.println("commitFileUpdate@ClientMain-ERROR-NULL:" + "basename=" + incomplete.basename + " parent=" + incomplete.parent + " globalCounter=" + incomplete.globalCounter + " status=" + incomplete.status);
+			} else {
+				System.out.println("commitFileUpdate@ClientMain-ERROR-NULL:" + "basename=" + incomplete.basename + " parent=" + incomplete.parent
+						+ " globalCounter=" + incomplete.globalCounter + " status=" + incomplete.status);
 				return false;
 			}
 		}
 	}
 
-	
 	public static void main(String[] args) {
-		
+
 		boolean suc = initClientMain();
-		
+
 		System.out.println("main@ClientMain=>" + suc);
 	}
-	
+
 }
